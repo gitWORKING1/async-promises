29f5ab1627a3f9945f8f97e22982c6497532d0d6
gitWORKING1 <cillian.murphy.phasma@gmail.com>
2025-05-21 18:47:18 +0300

    task 2 done

---
+++ task2/2-snackbar.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const delay = parseInt(form.elements['delay'].value);
+  const state = form.elements['state'].value;
+
+  createPromise(delay, state)
+    .then((delay) => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
